6d5e90f8493781d1e6f8074ccf8cfc2a2580076e
PavleMrva <pavlemrvaljevic1@gmail.com>
2024-04-20 23:19:18 +0200

    added keymap for k9s floaterm

--- nvim/lua/config/remaps/floaterm.lua
+++ nvim/lua/config/remaps/floaterm.lua
@@ -1,1 +1,2 @@
 vim.keymap.set("n", "<leader>lg", "<cmd>FloatermNew lazygit <CR>", { desc = "Toggle [L]azy [G]it" })
+vim.keymap.set("n", "<leader>ks", "<cmd>FloatermNew k9s <CR>", { desc = "Toggle [K]9[S]" })
